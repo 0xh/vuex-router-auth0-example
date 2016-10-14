5f24a044e50fad6ccae7b2dd7ff5c074bfab6b51
ridermansb <ridermansb@gmail.com>
2016-10-14 16:29:32 -0300

    Add Router

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h2>In Login</h2>
+    </div>
+</template>
+
+<style>
+</style>
+
+<script>
+    export default{
+        data(){
+            return{
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import dashboard from './components/Dashboard'
+import login from './components/Login'
 
-/* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/dashboard', component: dashboard },
+  { path: '/login', component: login }
+]
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
